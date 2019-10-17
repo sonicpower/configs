2e602812db44839eabcc836e2e40f0a66a42f632
sonicpower <56570653+sonicpower@users.noreply.github.com>
2019-10-17 11:24:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ if [ -f "$HOME/.vim/autoload/pathogen.vim" ]; then
 else
   echo "Installing pathogen"
   mkdir -p ~/.vim/autoload ~/.vim/bundle
-  curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  curl $(fwdproxy-config curl) -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
 
 # install vim solarized
@@ -66,7 +66,7 @@ if [ -d "$HOME/.vim/bundle/vim-colors-solarized" ]; then
   echo "Vim Solarized is already installed"
 else
   echo "Installing Vim Solarized"
-  git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 fi
 
 # install nerdtree
@@ -74,7 +74,7 @@ if [ -d "$HOME/.vim/bundle/nerdtree" ]; then
   echo "Nerdtree already installed"
 else
   echo "Installing Nerdtree"
-  git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 fi
 
 # install nerdcommenter
@@ -82,7 +82,7 @@ if [ -d "$HOME/.vim/bundle/nerdcommenter" ]; then
   echo "nerdcommenter already installed"
 else
   echo "Installing nerdcommenter"
-  git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 fi
 
 # install vim-fugitive
@@ -90,21 +90,21 @@ if [ -d "$HOME/.vim/bundle/vim-fugitive" ]; then
   echo "vim-fugitive already installed"
 else
   echo "Installing vim-fugitive"
-  git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 fi
 
 if [ -d "$HOME/.vim/bundle/ctrlp.vim" ]; then
   echo "ctrlp already installed"
 else
   echo "Installing ctrlp"
-  git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 fi
 
 if [ -d "$HOME/.oh-my-zsh" ]; then
   echo "oh-my-zsh already installed"
 else
   echo "Installing oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sh -c "$(curl $(fwdproxy-config curl) -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
@@ -113,30 +113,30 @@ if [ -d "$ZSH_CUSTOM/themes/powerlevel9k" ]; then
   echo "powerlevel9k already installed"
 else
   echo "Installing powerlevel9k"
-  git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
 fi
 
 if [ -d "$ZSH_CUSTOM/themes/zsh-autosuggestions" ]; then
   echo "zsh-autosuggestions already installed"
 else
   echo "Installing zsh-autosuggestions"
-  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
 if [ -d "$ZSH_CUSTOM/themes/zsh-syntax-highlighting" ]; then
   echo "zsh-syntax-highlighting already installed"
 else
   echo "Installing zsh-syntax-highlighting"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git -c https_proxy=http://regional-fwdproxy6-shv-01.rash0.facebook.com:8080 clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
 
 # copy .vimrc from my github
-curl -LSso ~/.vimrc https://raw.github.com/sonicpower/configs/master/.vimrc
+curl $(fwdproxy-config curl) -LSso ~/.vimrc https://raw.github.com/sonicpower/configs/master/.vimrc
 
 # copy aliases from my github
-curl -LSso $ZSH_CUSTOM/.zsh_aliases https://raw.github.com/sonicpower/configs/master/.zsh_aliases
+curl $(fwdproxy-config curl) -LSso $ZSH_CUSTOM/.zsh_aliases https://raw.github.com/sonicpower/configs/master/.zsh_aliases
 
 # copy .zshrc from my github
-curl -LSso ~/.zshrc https://raw.github.com/sonicpower/configs/master/.zshrc
+curl $(fwdproxy-config curl) -LSso ~/.zshrc https://raw.github.com/sonicpower/configs/master/.zshrc
 
 source ~/.zshrc
